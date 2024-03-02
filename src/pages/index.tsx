5cacb3dc2662383766948b50f6dae8facbcf3bdd
Vitaly Sivkov <vitaly.sivkov@gmail.com>
2024-03-02 18:40:00 +0100

    upd og information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
           property="og:description"
           content="Helping you to build and scale your product"
         />
-        <meta property="og:image" content="/og.png" />
+        <meta property="og:image" content="https://vitaly.so/og.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="vitaly.so" />
         <meta property="twitter:url" content="https://vitaly.so" />
@@ -48,7 +48,7 @@ export default function Home() {
           name="twitter:description"
           content="Helping you to build and scale your product"
         />
-        <meta name="twitter:image" content="/og.png" />
+        <meta name="twitter:image" content="https://vitaly.so/og.png" />
       </Head>
       <Header />
       <AboutMe />
