f650ea10dc598f2bbbd6491af86e046a53414b26
Vitaly Sivkov <vitaly.sivkov@gmail.com>
2024-03-04 13:18:35 +0100

    extract meta tags

---
+++ src/components/MetaTags.tsx
@@ -0,0 +1,34 @@
+export function MetaTags() {
+  return (
+    <>
+      <meta
+        name="description"
+        content="Helping to build and scale your product"
+      />
+
+      <meta property="og:url" content="https://vitaly.so" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:title"
+        content="👨🏻‍💻Vitaly Sivkov | Software developer"
+      />
+      <meta
+        property="og:description"
+        content="Helping to build and scale your product"
+      />
+      <meta property="og:image" content="https://vitaly.so/og.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="vitaly.so" />
+      <meta property="twitter:url" content="https://vitaly.so" />
+      <meta
+        name="twitter:title"
+        content="👨🏻‍💻Vitaly Sivkov | Full-stack developer"
+      />
+      <meta
+        name="twitter:description"
+        content="Helping to build and scale your product"
+      />
+      <meta name="twitter:image" content="https://vitaly.so/og.png" />
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { FeaturedWork } from "~/components/featured-work/FeaturedWork";
 import { HappyClients } from "~/components/happy-clients/HappyClients";
 import { ContactMe } from "~/components/contact-me/ContactMe";
 import { Footer } from "~/components/Footer";
+import { MetaTags } from "~/components/MetaTags";
 
 export default function Home() {
   return (
@@ -20,35 +21,8 @@ export default function Home() {
       }}
     >
       <Head>
-        <title>👨🏻‍💻Vitaly Sivkov | Software developer</title>
-        <meta
-          name="description"
-          content="Helping you to build and scale your product"
-        />
-
-        <meta property="og:url" content="https://vitaly.so" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="👨🏻‍💻Vitaly Sivkov | Software developer"
-        />
-        <meta
-          property="og:description"
-          content="Helping you to build and scale your product"
-        />
-        <meta property="og:image" content="https://vitaly.so/og.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="vitaly.so" />
-        <meta property="twitter:url" content="https://vitaly.so" />
-        <meta
-          name="twitter:title"
-          content="👨🏻‍💻Vitaly Sivkov | Software developer"
-        />
-        <meta
-          name="twitter:description"
-          content="Helping you to build and scale your product"
-        />
-        <meta name="twitter:image" content="https://vitaly.so/og.png" />
+        <title>👨🏻‍💻Vitaly Sivkov | Full-stack developer</title>
+        <MetaTags />
       </Head>
       <Header />
       <AboutMe />
