8246340ef0f3a22ab72263195362c1c91e9e5650
Vitaly Sivkov <vitaly.sivkov@gmail.com>
2024-03-05 12:51:43 +0100

    update meta

--- src/components/MetaTags.tsx
+++ src/components/MetaTags.tsx
@@ -10,25 +10,26 @@ export function MetaTags() {
       <meta property="og:type" content="website" />
       <meta
         property="og:title"
-        content="👨🏻‍💻Vitaly Sivkov | Software developer"
+        content="👨🏻‍💻Vitaly Sivkov | Full-stack Developer"
       />
       <meta
         property="og:description"
         content="Helping to build and scale your product"
       />
-      <meta property="og:image" content="https://vitaly.so/og.png" />
+      <meta property="og:image" content="https://vitaly.so/meta.jpg" />
+
       <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="vitaly.so" />
-      <meta property="twitter:url" content="https://vitaly.so" />
+      <meta name="twitter:site" content="@vitaly_so" />
       <meta
         name="twitter:title"
-        content="👨🏻‍💻Vitaly Sivkov | Full-stack developer"
+        content="👨🏻‍💻Vitaly Sivkov | Full-stack Developer"
       />
       <meta
         name="twitter:description"
         content="Helping to build and scale your product"
       />
-      <meta name="twitter:image" content="https://vitaly.so/og.png" />
+      <meta name="twitter:image" content="https://vitaly.so/meta.jpg" />
+      <meta name="twitter:url" content="https://vitaly.so" />
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,36 +33,8 @@ export default function Home() {
       }}
     >
       <Head>
-        <title>👨🏻‍💻Vitaly Sivkov | Full-stack developer</title>
-        <meta
-          name="description"
-          content="Helping to build and scale your product"
-        />
-
-        <meta property="og:url" content="https://vitaly.so" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="👨🏻‍💻Vitaly Sivkov | Software developer"
-        />
-        <meta
-          property="og:description"
-          content="Helping to build and scale your product"
-        />
-        <meta property="og:image" content="https://vitaly.so/meta.jpg" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@vitaly_so" />
-        <meta
-          name="twitter:title"
-          content="👨🏻‍💻Vitaly Sivkov | Full-stack developer"
-        />
-        <meta
-          name="twitter:description"
-          content="Helping to build and scale your product"
-        />
-        <meta name="twitter:image" content="https://vitaly.so/meta.jpg" />
-        <meta name="twitter:url" content="https://vitaly.so" />
+        <title>👨🏻‍💻Vitaly Sivkov | Full-stack Developer</title>
+        <MetaTags />
       </Head>
       <Header />
       <AboutMe />
