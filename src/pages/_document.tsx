653f5aa3b5e560b8189d808291b10aaea80c1b17
Vitaly Sivkov <vitaly.sivkov@gmail.com>
2024-11-15 16:39:11 +0100

    use openpanel analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { OpenPanelComponent } from "@openpanel/nextjs";
 
 export default function Document() {
   return (
@@ -10,11 +11,13 @@ export default function Document() {
       </Head>
       <body>
         {process.env.NODE_ENV === "production" && <SpeedInsights />}
-        <script
-          defer
-          src="/stats/script.js"
-          data-website-id="56322ae3-bb07-4025-b953-dc01b2bfdaa0"
-        ></script>
+        {process.env.NODE_ENV === "production" && (
+          <OpenPanelComponent
+            clientId="0984658d-9408-4a44-a78b-2c5825e4d0c9"
+            trackScreenViews={true}
+            trackOutgoingLinks={true}
+          />
+        )}
         <Main />
         <NextScript />
       </body>
